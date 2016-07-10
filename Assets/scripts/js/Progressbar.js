aafcf41f18a22e9a77738571d211249cf202c8c7
John <darkneter@gmail.com>
2016-07-10 10:03:30 -0700

    update progress bar.

--- Assets/scripts/js/Progressbar.js
+++ Assets/scripts/js/Progressbar.js
@@ -1,20 +1,26 @@
-﻿//#pragma strict
+﻿import UnityEngine.UI; // Force unity to laungh the UI components
+#pragma strict
 
 //var foregroundImage : UnityEngine.Transform;
 var foregroundImage:UnityEngine.UI.Image;
-var count :Number = 0;
+var count :float = 0;
+var bcount: boolean = true;
 
 function Start () {
-    foregroundImage = gameObject.GetComponent(UnityEngine.UI.Image) as UnityEngine.UI.Image;
+    //foregroundImage = gameObject.GetComponent(UnityEngine.UI.Image) as UnityEngine.UI.Image;
+    foregroundImage = gameObject.GetComponent(Image) as Image;
     Value(0);
 }
 
 function Value(value){
     
     if(foregroundImage !=null){
-        foregroundImage.fillAmount = value/100;
-        print(foregroundImage);    
-    return (parseInt)(foregroundImage.fillAmount*100);	
+        foregroundImage.fillAmount = value;
+        //print(foregroundImage);
+        //print(foregroundImage.fillAmount);
+        //print(value);
+        //return (foregroundImage.fillAmount = value);
+        return value;
     }
     else
     {
@@ -23,10 +29,11 @@ function Value(value){
 }
 
 function Update(){
-    count += 0.01;
-    if(count > 1){
-        count = 0;
+	if(bcount){
+	    count += 0.01;
+	    if(count > 1){
+	        count = 0;
+	    }
+	    Value(count);
     }
-    Value(count);
-    
 }
