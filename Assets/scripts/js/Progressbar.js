91cd42b62412d6bf278498ab456337f28efa7e15
John <darkneter@gmail.com>
2016-07-10 08:25:59 -0700

    HUD builds.

---
+++ Assets/scripts/js/Progressbar.js
@@ -0,0 +1,32 @@
+﻿//#pragma strict
+
+//var foregroundImage : UnityEngine.Transform;
+var foregroundImage:UnityEngine.UI.Image;
+var count :Number = 0;
+
+function Start () {
+    foregroundImage = gameObject.GetComponent(UnityEngine.UI.Image) as UnityEngine.UI.Image;
+    Value(0);
+}
+
+function Value(value){
+    
+    if(foregroundImage !=null){
+        foregroundImage.fillAmount = value/100;
+        print(foregroundImage);    
+    return (parseInt)(foregroundImage.fillAmount*100);	
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+function Update(){
+    count += 0.01;
+    if(count > 1){
+        count = 0;
+    }
+    Value(count);
+    
+}
