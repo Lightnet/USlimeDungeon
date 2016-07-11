aa2ef9c8e01b8b9009b30e9e026acd24282b69a0
John <darkneter@gmail.com>
2016-07-11 08:16:34 -0700

    dungeon code simple.

---
+++ Assets/scripts/js/CreatureStats.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var name:String = "";
+var gender:String;
+var races:String[];
+var title:String = "";
+var titles:String[];
+
+var traits:String[];
+var skills:String[];
+var passives:String[];
+
+var attack:float = 0;
+var defence:float = 0;
+
+var attackmagic:float = 0;
+var defencemagic:float = 0;
+
+var Strength:float = 0;
+var Endurance:float = 0;
+var Dexterity:float = 0;
+var Speed:float = 0;
+var Intelligence:float = 0;
+var Wisdom:float = 0;
+var Luck:float = 0;
+var Mentality:float = 100;
