8ea7004a77f9ca7e715c2b14eed0247e2ea7fffc
John <darkneter@gmail.com>
2016-08-07 10:19:03 -0700

    add and change to 2d object to 3d for pathing. But pathing doesn't do real time rebuilds.

---
+++ Assets/scripts/js/DrawCharacterCollision.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+//function Start () {
+
+//}
+
+//function Update () {
+
+//}
+
+#if UNITY_EDITOR
+ 
+ function OnDrawGizmos()
+ {
+     Gizmos.color = Color.yellow;
+     
+     //var col : BoxCollider = GetComponent.< BoxCollider >();
+     var col : CapsuleCollider = GetComponent.< CapsuleCollider >();
+     
+     if ( col )
+     {
+         Gizmos.matrix = transform.localToWorldMatrix;
+         //Gizmos.DrawWireCube( Vector3.zero + col.center, col.size );
+         //Gizmos.DrawWireCube( Vector3.zero + col.center, col.radius );
+         //Gizmos.DrawWireCube( Vector3.zero + col.center, 1 );
+         //Gizmos.DrawWireCube( col.center, new Vector3(1,1,1) );
+         //Gizmos.DrawSphere(col.center,col.radius);
+         Gizmos.DrawWireSphere(col.center,col.radius);
+
+     }
+ }
+     
+ #endif
